44ec3155304c792072d5857be32493fa23c4d567
Deepak1jha <ddjha171@gmail.com>
2021-09-21 00:00:38 +0530

    Initial commit

--- server.js
+++
@@ -1,9 +0,0 @@
-const express = require("express")
-const http = require("http")
-const app = express()
-const server = http.createServer(app)
-const socket = require("socket.io")
-const io = socket(server)
-
-server.listen(5000,()=>console.log("Server is running on port 5000"));
-
